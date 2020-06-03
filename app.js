888b980801f3a5fd617f065f1ae4fe6b054adf8d
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2020-06-03 13:49:17 +0200

    before push heroku

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const path = require("path")
-const express = require("express")
-const app = express()
-const router = require("./router")
-
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(express.static("public"))
-app.set("views", "views")
-app.set("view engine", "hbs")
-
-app.use("/", router)
+const path = require("path");
+const express = require("express");
+const app = express();
+const router = require("./router");
 
 
-app.listen(3000, () => {
-  console.log("The server is now running on Port 3000")
-})
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "hbs");
+
+app.use("/", router);
+
+
+app.listen(app.get('port'), () => {
+  console.log(`The server is now running `);
+});
